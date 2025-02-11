b2252a08195559c8f61349e8be335ad5f4aa38b1
helga2lip <helga2lip@gmail.com>
2025-02-11 11:02:04 +0300

    игра

--- src/Field/FieldLayout.jsx
+++ src/Field/FieldLayout.jsx
@@ -1,10 +1,15 @@
+import PropTypes from 'prop-types';
 import styles from './Field.module.css'
 
 export const FieldLayout = (props) => {
+    const onCellClick = (index) => {
+        props.onFieldClick(index)
+    }
+
     return <div className={styles.fieldLayout}>
         <div className={styles.fieldGrid}>
             {props.field.map((cell, index) => {
-                return <div key={index} className={styles.fieldCell}>
+                return <div key={index} className={styles.fieldCell} onClick={() => onCellClick(index)}>
                 {cell}
             </div>
             })}
@@ -12,3 +17,8 @@ export const FieldLayout = (props) => {
         </div>
     </div>
 }
+
+FieldLayout.propTypes = {
+    field: PropTypes.arrayOf(PropTypes.string),
+    onFieldClick: PropTypes.func,
+};
